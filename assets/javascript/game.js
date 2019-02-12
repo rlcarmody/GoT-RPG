90584f856249dec1db5d201d82ecb4718d016eb8
RLC <rlcarmody@gmail.com>
2019-02-11 20:10:31 -0800

    Changing theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,84 @@
 $(document).ready(function () {
 
     const player = {
-        remainingOpponents: 0,
-        attackMultiplier: 0
+        attackMultiplier: 0,
+        healthstatus: function () {
+            if(this.currentHP/this.baseHP > .6) {
+                return 'green';
+            }
+            else if (this.currentHP/this.baseHP >.2) {
+                return 'yellow';
+            }
+            return 'red';
+        }
     };
-    const opponent = {};
+    const opponent = {
+        healthstatus: function () {
+            if(this.currentHP/this.baseHP > .6) {
+                return 'green';
+            }
+            else if (this.currentHP/this.baseHP >.2) {
+                return 'yellow';
+            }
+            return 'red';
+        }
+    };
     var gameStart = false;
 
     const fighters = {
         fighter1: {
-            name: 'fighter1',
+            name: 'Daenarys',
             defeated: false,
             baseHP: 100,
             currentHP: 100,
             attackPower: function () {
                 return (10 - (this.baseHP - 100) / 10);
             },
-            counterAttackPower: function () {
-                return this.attackPower() * 2;
-            },
+            counterAttackPower: 25,
         },
         fighter2: {
-            name: 'fighter2',
+            name: 'The Mountain',
             defeated: false,
-            baseHP: 110,
-            currentHP: 110,
+            baseHP: 120,
+            currentHP: 120,
             attackPower: function () {
                 return 10 - (this.baseHP - 100) / 10;
             },
-            counterAttackPower: function () {
-                return this.attackPower() * 2;
-            },
+            counterAttackPower: 10
         },
         fighter3: {
-            name: 'fighter3',
+            name: 'Jon Snow',
             defeated: false,
             baseHP: 140,
             currentHP: 140,
             attackPower: function () {
                 return 10 - (this.baseHP - 100) / 10;
             },
-            counterAttackPower: function () {
-                return this.attackPower() * 2;
-            },
+            counterAttackPower: 5
         },
         fighter4: {
-            name: 'fighter4',
+            name: 'Khal Drogo',
             defeated: false,
             baseHP: 170,
             currentHP: 170,
             attackPower: function () {
                 return 10 - (this.baseHP - 100) / 10;
             },
-            counterAttackPower: function () {
-                return this.attackPower() * 2;
-            },
+            counterAttackPower: 20
         }
     }
     //console.log(Object.keys(fighters).length)
 
-    const loadFighters = () => {
+    const loadFighters = (jObj) => {
         let htmlString = '';
-        for (let prop in fighters) {
-            htmlString += `<img src="./assets/images/${fighters[prop].name}.png" alt="${fighters[prop].name}" class="fighter" value="${fighters[prop].name}"/><div class="healthbar">${fighters[prop].currentHP}</div>`
+        let keys = Object.keys(fighters);
+        for (let prop of keys) {
+            htmlString += `<div class="card fighter" value="${prop}"><h2>${fighters[prop].name}</h2><img src="./assets/images/${fighters[prop].name}.gif" alt="${fighters[prop].name}"/><div class="healthbar">${fighters[prop].currentHP}</div></div>`
         }
-        $('.allfighters').html(htmlString);
+        $(jObj).html(htmlString);
     }
 
-    loadFighters();
+    loadFighters('.allfighters');
 
     $('body').on('click', '.fighter', function () {
         let val = $(this).attr('value');
@@ -75,13 +86,12 @@ $(document).ready(function () {
             startGame(val);
             return;
         }
-        if ($('.opponent').html() !== ''){
+        if ($('.opponent').html() !== '') {
             return;
         }
-        Object.assign(opponent,fighters[val]);
+        Object.assign(opponent, fighters[val]);
         delete fighters[val];
-        $('.opponent').html(`<img src="./assets/images/${val}.png" alt="${val}"/><div class="healthbar">${opponent.currentHP}</div>`);
-        $(`.fighter[value="${val}"]+div`).remove();
+        $('.opponent').html(`<h2>${opponent.name}</h2><img src="./assets/images/${opponent.name}.gif" alt="${opponent.name}"/><div class="healthbar">${opponent.currentHP}</div>`);
         $(`.fighter[value="${val}"]`).remove();
         $('button').removeAttr('disabled');
     })
@@ -89,26 +99,37 @@ $(document).ready(function () {
     const startGame = (val) => {
         gameStart = true;
         $('.allfighters').empty();
-        $('.player').html(`<img src="./assets/images/${val}.png" alt="${val}"/><div class="healthbar">${fighters[val].currentHP}</div>`);
+        $('.player').html(`<h2>${fighters[val].name}</h2><img src="./assets/images/${fighters[val].name}.gif" alt="${fighters[val].name}"/><div class="healthbar">${fighters[val].currentHP}</div>`);
         Object.assign(player, fighters[val]);
         delete fighters[val];
-        let htmlString = '';
-        for (let prop in fighters) {
-            htmlString += `<img src="./assets/images/${fighters[prop].name}.png" alt="${fighters[prop].name}" class="fighter" value="${fighters[prop].name}"/><div class="healthbar">${fighters[prop].currentHP}</div>`
-        }
-        $('.enemies').html(htmlString);
+        loadFighters('.enemies');
     }
 
     $('.attack').on('click', function () {
+        $('#attack').trigger('play');
         player.attackMultiplier++;
-        opponent.currentHP -= player.attackPower() * player.attackMultiplier;
-        $('.opponent div').text(opponent.currentHP);
+        let damage = player.attackPower() * player.attackMultiplier;
+        opponent.currentHP -= damage;
+        $('.opponent div').text(opponent.currentHP).attr('hp',opponent.healthstatus());;
+        $('.opponent div').animate({width: ((opponent.currentHP/opponent.baseHP)*100).toString() + '%'})
         //todo check if opponent still has hp
-        player.currentHP -= opponent.counterAttackPower();
-        $('.player div').text(player.currentHP);
+        if (opponent.currentHP <= 0) {
+            alert (opponent.name + 'has been defeated');
+            $('.opponent').empty();
+            $('.attack').attr('disabled',true)
+        }
+        else {player.currentHP -= opponent.counterAttackPower;
+        $('.player div').text(player.currentHP).attr('hp',player.healthstatus());
+        $('.player div').animate({width: ((player.currentHP/player.baseHP)*100).toString() + '%'})
+
+        }
+         if (player.currentHP <= 0) {
+            $('.attack').attr('disabled',true);
+            return alert('You lose');
+        }
     })
 
-    
+
 
 })
 // listener for class of all fighters
